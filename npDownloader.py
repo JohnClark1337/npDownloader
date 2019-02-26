13a44f3a6183c413cca65fde02efec882f78a9d6
JScofield <jdsco37@gmail.com>
2019-02-26 16:59:28 -0500

    Add files via upload
    Downloads all nuklearpower.com comics to an 8-Bit Theater folder in documents. Still in testing.

---
+++ npDownloader.py
@@ -0,0 +1,50 @@
+"""
+NPDownloader.py
+Created by Jonathon Scofield
+
+Designed to download all comics from the 8-bit theater on nuklearpower.com
+Possibly will expand to further download capability later
+
+"""
+
+
+import os
+from bs4 import BeautifulSoup
+import wget
+import urllib
+import re
+
+
+dirname = "8-bit Theater"
+initialLink = 'http://www.nuklearpower.com/2001/03/02/episode-001-were-going-where/'
+
+pathname = os.path.expanduser('~') + "\\Documents\\" + dirname
+if os.path.exists(pathname) == False:
+    os.makedirs(pathname)
+
+looper = True
+epnum = 1
+while looper:
+    
+    content = urllib.request.urlopen(initialLink).read()
+
+    soup = BeautifulSoup(content, features="html5lib")
+    for image in soup.find_all('img'):
+        check = image.get('alt')
+        if "Episode" in check:
+            ep = image.get('src')
+            print(ep)
+            dloc = pathname + "\\" + str(epnum) + '.jpg'
+            epnum += 1
+            wget.download(ep, dloc)
+
+    for links in soup.find_all('a'):
+        checknext = str(links.get('rel'))
+        if "next" in checknext:
+            initialLink = links.get('href')
+
+print("Series Complete")
+
+
+
+
